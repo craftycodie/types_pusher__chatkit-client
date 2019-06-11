708bf535668f598cdd47d6998f5e5d7561cbeb6e
Alex Newark <git@alexnewark.co.uk>
2019-06-11 12:57:46 +0100

    Module and classes.

--- index.d.ts
+++ index.d.ts
@@ -4,18 +4,24 @@
 // Definitions: https://github.com/Alex-231/types_pusher__chatkit-client
 
 
-export interface CurrentUser {
-    sendMessage: (message: { text: string, roomId: string }) => Promise<void>;
-    subscribeToRoom: (options: { roomId: string, hooks?: { 
-        onMessage: (data: { id: string, senderId: string, text: string, createdAt: string }) => any }
-    }) => void;
-}
-
-export interface ChatManager {
-    constructor(options: { instanceLocator: string, userId: string, tokenProvider: TokenProvider }): void,
-    connect: () => Promise<CurrentUser>,
-}
-
-export interface TokenProvider {
-    constructor(options: { url: string }): void,
-}
+declare module "@pusher/chatkit-client" {
+    class CurrentUser {
+      sendMessage: (message: { text: string, roomId: string }) => Promise<void>;
+      subscribeToRoom: (options: {
+        roomId: string, hooks?: {
+          onMessage: (data: { id: string, senderId: string, text: string, createdAt: string }) => any
+        }
+      }) => void;
+    }
+  
+    class ChatManager {
+      constructor(options: { instanceLocator: string, userId: string, tokenProvider: TokenProvider });
+  
+      connect: () => Promise<CurrentUser>;
+    }
+  
+    class TokenProvider {
+      constructor(options: { url: string });
+    }
+  }
+  
